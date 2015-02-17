74552dd83a8988577fe8990f84edfb57654698bc
Atsushi Kokubu <akokubu@longposition.co.jp>
2015-02-17 09:08:15 +0900

    サービス

--- src/main/resources/static/app/sample_controller.js
+++ src/main/resources/static/app/sample_controller.js
@@ -11,6 +11,19 @@
             info: false
         };
 
+        $scope.message = {
+            type: 'alert-info',
+            text: '',
+            show: false
+        };
+
+        $scope.showMessage = function(msg) {
+            $scope.message = msg;
+            $timeout(function() {
+                $scope.message.show = false;
+            }, 3000);
+        };
+
         $scope.changePage = function(type) {
             for (var name in $scope.show) {
                 if (name == type) {
@@ -23,6 +36,14 @@
     });
 
     module.controller('listController', function($scope) {
+        $scope.show = function(item) {
+          $scope.$parent.changePage('info');
+        };
+
+        $scope.$on('changeItems', function() {
+            alert('一覧が更新されたました');
+        });
+
         $scope.items = [
             {
                 title: 'お買い物リスト',
@@ -33,5 +54,20 @@
                 memo: 'XX銀行の貯金通帳に記入する'
             }
         ];
+    });
+
+    module.controller('addController', function($scope) {
+       $scope.addItem = function() {
+
+           $scope.$parent.showMessage({
+               type: 'alert-inf',
+               text: '追加しました',
+               show: true
+           });
+
+           $scope.$root.$broadcast('changeItems');
+
+           $scope.$parent.changePage('list');
+       }
     });
 }(TodoModule));
